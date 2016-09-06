000cfa56f39a2972b95bf1f9520c0cc49897520b
KeimiOkamoto <keimiokamoto@gmail.com>
2016-09-06 22:12:23 +0100

    Adds another method

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -3,4 +3,9 @@ public class SomeClass {
     public void method() {
         System.out.println("Hello World");
     }
+
+    public void method1() {
+        System.out.println("Hello World again");
+    }
+
 }
